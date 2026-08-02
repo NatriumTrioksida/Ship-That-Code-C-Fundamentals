eba71800a9b846827f70bef7e6653cfa7c48d657
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 12:04:55 +0700

    lesson 2

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    // Print the greeting below.
-    printf("Hello, C!");
+	int a, b;
+	if (scanf("%d", &a) != 0) return 1;
+	if (scanf("%d", &b) != 0) return 1;
+	printf("%d\n", a + b);
     return 0;
 }
