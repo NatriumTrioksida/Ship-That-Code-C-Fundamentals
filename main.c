f73c2757a1f0a8aed7d0613abbab49a92dfb0117
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 12:07:26 +0700

    lesson 2

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main(void) {
 	int a, b;
-	if (scanf("%d", &a) != 0) return 1;
-	if (scanf("%d", &b) != 0) return 1;
+	if (!scanf("%d", &a)) return 1;
+	if (!scanf("%d", &b)) return 1;
 	printf("%d\n", a + b);
     return 0;
 }
