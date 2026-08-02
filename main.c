01ff9af80093a2c9851fb4a415113a65f8e71129
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 13:39:56 +0700

    lesson 6

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main(void) {
   }
   if (n % 15 == 0) {
     printf("FizzBuzz\n");
-  } else if (n % 3) {
-    printf("Buzz\n");
-  } else if (n % 5) {
+  } else if (n % 3 == 0) {
     printf("Fizz\n");
+  } else if (n % 5 == 0) {
+    printf("Buzz\n");
   } else {
     printf("%d\n", n);
   }
