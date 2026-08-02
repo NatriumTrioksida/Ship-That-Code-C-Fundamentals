b9db7cda6a31a06850adaefecf22f922531679b0
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 18:08:53 +0700

    lesson 11

--- main.c
+++ main.c
@@ -1,29 +1,36 @@
+#include <stddef.h>
 #include <stdio.h>
-
-typedef struct {
-  int x, y;
-} Point;
-
-int sqrDistance(Point a, Point b) {
-  int x = b.x - a.x;
-  int y = b.y - a.y;
-  return (x * x) + (y * y);
-}
+#include <stdlib.h>
 
 int main(void) {
-  Point a, b;
-  if (!scanf("%d", &a.x)) {
+  size_t size;
+  if (!scanf("%ld", &size)) {
     return 1;
   }
-  if (!scanf("%d", &a.y)) {
+
+  int *arr = malloc(size * sizeof(int));
+  if (!arr) {
     return 1;
   }
-  if (!scanf("%d", &b.x)) {
+
+  size_t read_fail = 0;
+  for (int i = 0; i < size; i++) {
+    if (!scanf("%d", &arr[i])) {
+      read_fail = 1;
+      break;
+    }
+  }
+  if (read_fail) {
+    free(arr);
     return 1;
   }
-  if (!scanf("%d", &b.y)) {
-    return 1;
+
+  long long sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += arr[i];
   }
-  printf("%d\n", sqrDistance(a, b));
+  printf("%lld\n", sum);
+
+  free(arr);
   return 0;
 }
