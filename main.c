57af8f4184b077d3aae1f94fc0988bdfb7db5bf4
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 14:10:55 +0700

    lesson 8

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+int square(int);
+
 int main(void) {
   int n;
   if (!scanf("%d", &n)) {
     return 1;
   };
-  long long total = 0;
-  for (int i = 1; i <= n; i++) {
-    total += i;
-  }
-  printf("%lld\n", total);
+  printf("%d\n", square(n));
   return 0;
+}
+
+int square(int n) {
+	return n * n;
 }
