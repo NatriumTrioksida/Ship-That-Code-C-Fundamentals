fff7c22d5b590bce97ac643b5dbeb8a8ff7cc960
natrium_trioxide <milan0r.the.silverwolf@gmail.com>
2026-08-02 17:45:41 +0700

    lesson 10

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+typedef struct {
+  int x, y;
+} Point;
+
+int sqrDistance(Point a, Point b) {
+  return (b.x * b.x - a.x * a.x) + (b.y * b.y - a.y * a.y);
 }
 
 int main(void) {
-  int a, b;
-  if (!scanf("%d %d", &a, &b)) {
+  Point a, b;
+  if (!scanf("%d", &a.x)) {
     return 1;
-  };
-  swap(&a, &b);
-  printf("%d %d\n", a, b);
+  }
+  if (!scanf("%d", &a.y)) {
+    return 1;
+  }
+  if (!scanf("%d", &b.x)) {
+    return 1;
+  }
+  if (!scanf("%d", &b.y)) {
+    return 1;
+  }
+  printf("%d\n", sqrDistance(a, b));
   return 0;
 }
